99aa0c74d9aadc4dfdb3a10b794ec4f0d30d69d6
Mark Shirley <mshirley@gmail.com>
2011-05-12 16:15:08 -0700

    adding logging features

---
+++ lib/logging.rb
@@ -0,0 +1,18 @@
+def write_log(projname, outfile, verbose, message)
+	File.open("./#{projname}/#{outfile}", 'a') { |file|
+		file.write("[#{Time.now}]" + " " + message + "\n")
+		if verbose == true
+			puts "[#{Time.now}]" + " " + message
+		end
+		file.close
+	}
+end
+
+def load_output(projname, outfile)
+	if File.directory?("./#{projname}")
+		puts "Directory already exists"
+	else
+		puts "Creating output directory"
+		Dir.mkdir("./#{projname}")
+	end
+end
